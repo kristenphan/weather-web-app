0d52e7c12f3ca809386f0f29b7fd9f4b97b41a13
Kristen (Thi Ha Giang) Phan <hgiang.phan@yahoo.com.vn>
2022-08-23 20:24:58 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Tutorial url: https://nordicapis.com/how-to-build-an-api-driven-weather-app/
 // OpenWeatherMap API
 const api = "1e5664d4684fd1be5667d4ec7583c068";
 
@@ -9,8 +10,6 @@ const tempF = document.querySelector('.f');
 const desc = document.querySelector('.desc');
 const sunriseDOM = document.querySelector('.sunrise');
 const sunsetDOM = document.querySelector('.sunset');
-// TO DELETE
-const iconURLDOM = document.querySelector('#iconUrl');
 
 // Using an event listener to run a function every time the page is loaded
 window.addEventListener('load', () => {
@@ -37,24 +36,19 @@ window.addEventListener('load', () => {
                     const { sunrise, sunset } = data.sys;
                     // Getting URL for the weather icon based in icon id "icon"
                     const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-                    var iconUrlVar = iconUrl;
                     const fahrenheit = (temp * 9) / 5 + 32; // Calculating fah from celcius
 
                     // Converting Epoch(Unix) time to GMT
                     const sunriseGMT = new Date(sunrise * 1000);
                     const sunsetGMT = new Date(sunset * 1000);
 
-                    // Interacting with DOM to show data
-                    //iconImg.src = iconUrl;
-                    // code below: https://www.youtube.com/watch?v=8R3FtApLdms
-                    $('#weather-icon').attr('src', iconUrl);
                     loc.textContent = `${place}`;
                     desc.textContent = `${description}`;
-                    tempC.textContent = `${temp.toFixed(2)} °C`;
-                    tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
+                    tempC.textContent = `${temp.toFixed(2)} Â°C`;
+                    tempF.textContent = `${fahrenheit.toFixed(2)} Â°F`;
                     sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
                     sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
-                    iconURLDOM.textContent = `${iconUrl}`;
+                    iconImg.src = iconUrl;
                 });
         });
     }
